bdc7c81afc9df4261042e948784871a6d00282f0
Daniel Rogers <boldanrogers@gmail.com>
2020-12-18 02:40:39 -0500

    3 challenges complete

--- Challenge2Claims_Tests/Challenge2ClaimsTest.cs
+++ Challenge2Claims_Tests/Challenge2ClaimsTest.cs
@@ -26,7 +26,7 @@ namespace Challenge2Claims_Tests
 
         [TestMethod]
 
-        public void ReturnQueue_SouldBeLargerThanOne()
+        public void ReturnQueue_SouldBeLargerThanZero()
         {
             Queue<Claim> aQueue = _claimRepo.ReturnQueue();
             int size = aQueue.Count;
@@ -45,6 +45,52 @@ namespace Challenge2Claims_Tests
 
         }//end of method Add_ShouldBeTrue
 
+        //peekclaim
+        [TestMethod]
+        public void PeekClaim_ShouldNotBeNull()
+        {
+            _aClaim = _claimRepo.PeekClaim();
+            Assert.IsNotNull(_aClaim);
 
-    }
+        }//end of test PeekClaim
+
+        //dequeueclaim
+        [TestMethod]
+        public void DequeueClaim_ShouldBeFalse()
+        {
+            //peek to get it, dequeue it, check if the queue contains it still
+            _aClaim = _claimRepo.PeekClaim();
+            _claimRepo.DequeueClaim();
+            Assert.IsFalse(_claimRepo.ContainsClaim(_aClaim));
+
+
+        }//end of test DequeueClaim_Should BeFalse
+
+
+
+        //contains
+        public void Contains_ShouldBeTrue()
+        {
+            //maybe opposite then
+            //check that it doesnt contain a claim
+            //add it and then check again
+            //so create the new claim
+            _aClaim = new Claim(4, ClaimType.Home, "tree fell on garage", 7000.00f, new DateTime(2020, 1, 2), new DateTime(2020, 5, 9), false);
+            //check that it isnt there yet
+            bool status = _claimRepo.ContainsClaim(_aClaim);
+            //should return false, now add the claim
+            _claimRepo.Add(_aClaim);
+            //and a new status
+            bool newStatus = _claimRepo.ContainsClaim(_aClaim);
+            //whew if we do this right it will say the first failed and the second passed
+            Assert.IsTrue(newStatus && !status);
+
+
+
+        }//end of Contains_ShouldBeTrue
+
+
+
+
+    }//end of class
 }
--- Challenge3Badge_Tests/Challenge3Badge_Tests.cs
+++ Challenge3Badge_Tests/Challenge3Badge_Tests.cs
@@ -1,14 +1,92 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using Challenge3Badge_Repository;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
+using System.Collections.Generic;
 
 namespace Challenge3Badge_Tests
 {
     [TestClass]
-    public class UnitTest1
+    public class Challenge3Badge_Tests
     {
-        [TestMethod]
-        public void TestMethod1()
+
+        //we need our private members
+        Badge _aBadge = new Badge();
+        BadgeRepository _badgeRepo = new BadgeRepository();
+        //then we need our arrange method
+        [TestInitialize]
+        public void Arrange()
         {
-        }
-    }
+
+
+            Console.WriteLine("inside arrange");
+            _aBadge = new Badge(1, new List<string>() { "A4", "B1", "C7" });
+            _badgeRepo.Add(_aBadge);
+            _aBadge = new Badge(2, new List<string>() { "A1", "A2", "A3", "A4" });
+            _badgeRepo.Add(_aBadge);
+            _aBadge = new Badge(3, new List<string>() { "C5", "B2", "B1", "A1" });
+            _badgeRepo.Add(_aBadge);
+            
+
+
+        }//end of method Arrange
+
+        //then we need to test
+
+        //add return, return update delete
+
+        /*[TestMethod]
+        public void TestTest()
+        {
+            Assert.AreEqual(true, true);
+        }//end of test
+        */
+
+        
+        [TestMethod]
+        public void Add_ShouldReturnTrue()
+        {
+            _aBadge = new Badge(4, new List<string>() { "R2", "D2", "C3", "P0" });
+            _badgeRepo.Add(_aBadge);
+            Assert.IsNotNull(_badgeRepo.ReturnBadge(_aBadge.BadgeID));
+
+
+        }//end of add
+        [TestMethod]
+        public void ReturnDictionary_ShouldNotBeNull()
+        {
+            Dictionary<int, Badge> aDictionary = _badgeRepo.ReturnDictionary();
+            Assert.IsNotNull(aDictionary);
+
+        }//end of returnQueue
+        [TestMethod]
+        public void ReturnBadge_ShouldBeEqual()
+        {
+            _aBadge = new Badge(4, new List<string>() { "R2", "D2", "C3", "P0" });
+            _badgeRepo.Add(_aBadge);
+            Assert.IsNotNull(_badgeRepo.ReturnBadge(_aBadge.BadgeID));
+
+        }//end of ReturnBadge
+        [TestMethod]
+        public void Update_ShouldNotEqual()
+        {
+            //we need to get the badge and save the list, change the badge, get the badge
+            //and save its list, and compare and assert 
+            List<string> aList = _badgeRepo.ReturnBadge(1).DoorList;
+            _aBadge.DoorList = new List<string>() { "C5", "B2", "B1", "A1" };
+            _badgeRepo.UpdateBadge(1, _aBadge);
+            Assert.AreNotEqual(aList, _badgeRepo.ReturnBadge(1).DoorList);
+
+        }//end of Update_ShouldBeEqual
+        [TestMethod]
+        public void Delete_ShouldBeTrue()
+        {
+            Assert.AreEqual(true, _badgeRepo.Delete(1));
+        }//end of DeleteDoors
+        
+
+
+
+
+
+    }//end of class
 }
